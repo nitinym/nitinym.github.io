d312c13facd268e33a1d96799f67988d8617a293
nitinym <71812771+nitinym@users.noreply.github.com>
2021-05-21 12:07:37 +0530

    Update change-close-icon.js

--- change-close-icon.js
+++ change-close-icon.js
@@ -1,7 +1,17 @@
 
-function closeBot() {
-    document.getElementsByClassName("close-chat")[0].src =
-        "https://cdn.yellowmessenger.com/DijpOqYkPF8A1621572136172.png"
-}
+// function closeBot() {
+//     document.getElementsByClassName("close-chat")[0].src =
+//         "https://cdn.yellowmessenger.com/DijpOqYkPF8A1621572136172.png"
+// }
 
-setTimeout(closeBot(), 3000)
+// setTimeout(closeBot(), 3000)
+
+const styles = `
+    .close-chat {
+        content:url("https://cdn.yellowmessenger.com/DijpOqYkPF8A1621572136172.png");
+    }
+`
+const frameDocument  = window.frames && window.frames.ymIframe && window.frames.ymIframe.document
+var styleSheet = document.createElement("style")
+styleSheet.innerText = styles
+frameDocument.head.appendChild(styleSheet);
