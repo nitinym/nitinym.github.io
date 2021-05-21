e48bfc21eaba9b29588be5d0c0c6169b03a7db79
nitinym <71812771+nitinym@users.noreply.github.com>
2021-05-21 12:23:08 +0530

    re-size

--- change-close-icon.js
+++ change-close-icon.js
@@ -10,6 +10,15 @@ const styles = `
     .close-chat {
         content:url("https://cdn.yellowmessenger.com/DijpOqYkPF8A1621572136172.png");
     }
+    @media screen and (max-device-width: 768px) {
+        #chatDetails .back img {
+        width: 15px;
+        height: 15px;
+        border-radius: 0;
+        margin: -5px 20px 10px;
+        cursor: pointer;
+        }
+    }
 `
 const frameDocument  = window.frames && window.frames.ymIframe && window.frames.ymIframe.document
 var styleSheet = document.createElement("style")
