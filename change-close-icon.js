9e12bb2fb765ec73dc3850f61631e002e9be1097
globefire <sahun5774@gmail.com>
2021-05-21 10:14:10 +0530

    close bot icon

---
+++ change-close-icon.js
@@ -0,0 +1,7 @@
+
+function closeBot() {
+    document.getElementsByClassName("close-chat")[0].src =
+        "https://cdn.yellowmessenger.com/DijpOqYkPF8A1621572136172.png"
+}
+
+setTimeout(closeBot(), 1000)
